e930eaeb3b24cc95f01927630c727e54bd5369d5
binarypie <code@binarypie.com>
2026-01-01 13:27:49 -0800

    remove some things from fish

--- dot_files/fish/config.fish
+++ dot_files/fish/config.fish
@@ -11,18 +11,10 @@ if status is-interactive
     set -gx STARSHIP_CONFIG /usr/share/hypercube/config/starship/starship.toml
     starship init fish | source
 
-    # Env Variables
-    set -gx GOOGLE_CLOUD_PROJECT ibexio-src
-
     # Path
     fish_add_path $HOME/.local/bin
     fish_add_path $HOME/.npm-packages/bin
     fish_add_path $HOME/.cargo/bin
-    fish_add_path $HOME/Code/flutter/bin
     fish_add_path $HOME/go/bin
 
 end
-
-### bling.fish source start
-test -f /usr/share/ublue-os/bluefin-cli/bling.fish && source /usr/share/ublue-os/bluefin-cli/bling.fish
-### bling.fish source end
